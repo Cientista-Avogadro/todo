90bef7df4982219d1d1b27c7f7b7bdb7a46c8114
cientista-avogadro <cientistaavogadro2019@gmail.com>
2021-12-20 16:09:20 +0100

    trabalhando com objectos complexos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public todos: any[] = [];
+  public todos: Todo[] = [];
   public title: String = 'Minha Lista de Tarefa';
 
   constructor() {
-    this.todos.push('ir ao supermercado');
-    this.todos.push('comprar pão');
-    this.todos.push('cortar o cabelo');
+    this.todos.push(new Todo(1, 'ir ao supermercado', false));
+    this.todos.push(new Todo(2, 'comprar pão', false));
+    this.todos.push(new Todo(3, 'cortar o cabelo', false));
+    console.log(this.todos);
   }
 }
---
+++ src/models/todo.model.ts
@@ -0,0 +1,3 @@
+export class Todo {
+  constructor(public id: Number, public tile: String, public done: Boolean) {}
+}
